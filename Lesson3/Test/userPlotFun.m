75808f0211901ff25e2f333ae2345bcea27e5fc4
idowein <ido.weinstock@gmail.com>
2025-03-19 17:06:54 +0200

    coding task 3

--- Lesson3/Test/userPlotFun.m
+++ Lesson3/Test/userPlotFun.m
@@ -1,6 +1,13 @@
 % 211677083
 
-function userPlotFun
+function userPlotFun()
     
+    % 3.2 get input from user
+    start_time = input('please enter timeVector start: ');
+    end_time = input('please enter timeVector end: ');
+    num_points = input('please enter number of samples: ');
+    
+    % 3.3 
+    PlotFun(start_time, end_time, num_points);
 
 end
