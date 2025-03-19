47c0ab43e94badd872b39d3f9bd825a43b227242
idowein <ido.weinstock@gmail.com>
2025-03-19 10:33:14 +0200

    coding question 1

--- Lesson2/ID211677083.m
+++ Lesson2/ID211677083.m
@@ -1,7 +1,9 @@
 %% Part I
-%% Q 1
+%% Q1
 disp(((4^2)/(4+5^2)+(6/75))^(2/3))
 disp((1+9i)+((4+7i)+(31+8i))^4)
 disp((cos(60))^3+(tand(12.5)^5)) %% notice that the cosine in radians and tangens in degrees
 disp((((7*10^-3)+8)^0.5)*12.5/5.3)
-disp
+disp(abs((sind(45))*4^(4*pi+cosh(log2(pi^4)))))
+
+%% Q2
